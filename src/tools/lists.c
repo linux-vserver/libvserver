76857bafe5298144fd7a68f4b844997ec78529e3
Benedikt Böhm <bb@xnull.de>
2007-02-03 23:45:31 +0000

    get rid of config.h

--- src/tools/lists.c
+++ src/tools/lists.c
@@ -15,14 +15,8 @@
 // Free Software Foundation, Inc.,
 // 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <stdlib.h>
 #include <sys/resource.h>
-
-#include "linux/capability.h"
 
 #include "tools.h"
 #include "vserver.h"
