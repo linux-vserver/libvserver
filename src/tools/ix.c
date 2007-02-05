9ee1dbebf14a24488a92512769820b21f878a62c
Benedikt Böhm <bb@xnull.de>
2007-02-05 16:41:18 +0000

    fix latest api changes for the log multiplexer

--- src/tools/ix.c
+++ src/tools/ix.c
@@ -64,8 +64,9 @@ int main(int argc, char *argv[])
 
 	/* logging */
 	log_options_t log_options = {
-		.ident  = argv[0],
-		.stderr = true,
+		.log_ident = argv[0],
+		.log_dest  = LOGD_STDERR,
+		.log_opts  = LOGO_PRIO|LOGO_IDENT,
 	};
 
 	log_init(&log_options);
--- src/tools/vr.c
+++ src/tools/vr.c
@@ -63,8 +63,9 @@ int main(int argc, char *argv[])
 
 	/* logging */
 	log_options_t log_options = {
-		.ident  = argv[0],
-		.stderr = true,
+		.log_ident = argv[0],
+		.log_dest  = LOGD_STDERR,
+		.log_opts  = LOGO_PRIO|LOGO_IDENT,
 	};
 
 	log_init(&log_options);
